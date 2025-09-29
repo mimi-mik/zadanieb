80a4c7e7e3ac02ec34da32d11bc4156d1cd954d2
mimi-mik <mimigadulka@gmail.com>
2025-09-29 19:23:51 +0200

    Poprawiłem i dodałem kilka funkcji.

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include <stdlib.h>
 
@@ -12,15 +11,21 @@ int main()
     while(1)
     {
      printf("Zgadnij liczbe od 1 do 100: \n");
-     scanf("%d", odpowiedz);
+     scanf("%d", &odpowiedz);
      if(odpowiedz == number)
      {
          printf("Brawo zgadles poprawna liczba to: %d", number);
+         getchar();
+         getchar();
          exit(0);
      }
-     else
+     else if(odpowiedz < number)
      {
-         printf("Nie poprawna liczba zgaduj dalej!!! \n");
+         printf("Za malo! \n");
+     }
+     else if(odpowiedz > number)
+     {
+         printf("Za duzo!");
      }
     }
     return 0;
