e69db845a7244e2f2f7f047374646f8f1e6c9df8
mimi-mik <mimigadulka@gmail.com>
2025-09-29 17:49:45 +0200

    Initial commit - dodano pliki i kod programu(bez bibliotek trzeba samemu zainstalować MinGW)

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    int odpowiedz;
+    srand(time(NULL));
+    int number = rand() % 100;
+    //printf("%d", number);
+    while(1)
+    {
+     printf("Zgadnij liczbe od 1 do 100: \n");
+     scanf("%d", odpowiedz);
+     if(odpowiedz == number)
+     {
+         printf("Brawo zgad³es poprawna liczba to: %d", number);
+         exit(0);
+     }
+     else
+     {
+         printf("Nie poprawna liczba zgaduj dalej!!! \n");
+     }
+    }
+    return 0;
+}
